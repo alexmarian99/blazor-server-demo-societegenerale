a8383b1e76b0b3d4818eaf5d5af1f0cd84d8e76a
Alexandru Buza <alexandrumarianbuza@gmail.com>
2022-11-15 16:47:29 +0200

    Add project

---
+++ ChatSystem.cs
@@ -0,0 +1,32 @@
+﻿namespace BlazorDemo
+{
+    public delegate void NotifyUpdateUi();
+    public class ChatSystem
+    {
+        public List<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
+        public event NotifyUpdateUi UpdateUiEvent;
+
+        public void AddMessage(string Author, string Content)
+        {
+            Messages.Add(new ChatMessage()
+            {
+                Name = Author,
+                Message = Content
+            });
+            try
+            {
+                UpdateUiEvent.Invoke();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+
+    public class ChatMessage
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
---
+++ PresentationReview.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorDemo
+{
+    public class PresentationReview
+    {
+        public List<int> GradesList { get; set; } = new List<int>();
+        public event NotifyUpdateUi UpdateUiEvent;
+
+        public void AddGrade(int value)
+        {
+            GradesList.Add(value);
+            try
+            {
+                UpdateUiEvent.Invoke();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
